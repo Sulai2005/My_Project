a3e46852039a0067c169a3393bb5f94a688c2aba
Sulai2005 <technogamerz6693@gmail.com>
2025-02-10 09:03:48 +0530

    fix: compressed the stopwords

--- text classifier using a model/SSP001.py
+++ text classifier using a model/SSP001.py
@@ -2,13 +2,14 @@ import pandas as pd
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import LabelEncoder
-
 import re
+from nltk.corpus import stopwords
+import nltk
 
 class Text_Classifer:
     def __init__(self, filename_work, filename_train):
-        self.stop_words = {'aren', 'once', 'herself', "it's", 'what', 'whom', 'shouldn', 'have', 'now', 'most', 'no', 'her', 'own', 'itself', 've', "haven't", "mightn't", 'as', 'again', 'under', 'his', 'such', 'having', 'needn', 'had', 'a', 'in', 'were', 'when', 'y', 'doesn', 'an', 'more', 'but', "you're", 'all', 'into', 'or', 'for', 'ain', 'wasn', 'each', 'she', 'o', "isn't", 'before', 'is', "mustn't", "you'll", 'mightn', "doesn't", 'won', 'if', 'only', 'did', 'then', 'hadn', 'ma', "she's", 'with', "needn't", 'further', 'd', "couldn't", 'to', 'against', 'wouldn', 'couldn', 'does', 'same', 'themselves', 'both', 'should', 'weren', 'me', "weren't", 'down', "should've", 'yourself', "hadn't", 'the', 'because', 'during', 'about', 'below', 'will', "wouldn't", 'from', 'which', "didn't", 'my', 'than', "hasn't", 'above', 'him', 're', 'between', 'up', 'over', 'too', 'those', 'shan', 'm', 'hasn', 'doing', "shouldn't", 'these', 'few', 'out', 'yourselves', 'do', 'was', "won't", 'them', 'ourselves', 'i', 'there', 'can', 'we', 'yours', 'you', 'that', 'he', 'himself', 'being', 'so', 'they', 'didn', 'here', 'by', 'off', 'who', 'its', 'don', 'of', 'your', 'through', 'just', "aren't", 'their', 'some', 'nor', 'our', 'why', 'am', 'hers', 'at', 'until', "you've", 'other', 'ours', "you'd", 'on', 't', 'this', 'and', 'myself', 'after', 'it', 'very', 'any', "that'll", 'be', 'has', 'while', 's', "don't", 'not', 'theirs', 'how', 'haven', 'mustn', "wasn't", 'are', 'll', 'isn', 'where', 'been', "shan't"}
+        nltk.download('stopwords')
+        self.stop_words = set(stopwords.words('english'))
         self.topics = ['Legal', 'Governance', 'Social', 'Reputational Risk', 'Regulatory Risk', 'Physical Risk', 'Transition Risk', 'Waste and Water', 'Biodiversity', 'Fossil Fuels']
         self.filename_train = filename_train
         self.filename_work = filename_work
